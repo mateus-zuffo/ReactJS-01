1eb6efc2c31c9ed2d274b8eb468dae47a1b2f509
Mateus Zuffo <mateushmdz@gmail.com>
2023-03-10 23:30:32 -0300

    created a new component and style files

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,7 @@
 import DefaultExport from "./DefaultExport";
 import { NamedExport } from "./NamedExport";
+import { Post } from "./Post";
+import "./style.css";
 
 function App() {
 
@@ -9,6 +11,14 @@ function App() {
       <DefaultExport/>
       <NamedExport/>
       <NamedExport/>
+      <Post
+        author="Mateus Zuffo"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur in nemo voluptas ut eius laboriosam perspiciatis molestias repellendus inventore corrupti velit, nobis voluptates officia ipsam vero, expedita earum, qui culpa?"
+      />
+      <Post
+        author="Mateus Zuffo"
+        content="Teste ipsum dolor sit amet consectetur adipisicing elit. Pariatur in nemo voluptas ut eius laboriosam perspiciatis molestias repellendus inventore corrupti velit, nobis voluptates officia ipsam vero, expedita earum, qui culpa?"
+      />
     </div>
   )
 }
