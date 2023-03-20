f465fe1be7b9f13a6eb5236a4a13e5e3a9839d8d
Mateus Zuffo <mateushmdz@gmail.com>
2023-03-20 20:34:28 -0300

    feat: sidebar almost finished

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,7 @@
-import { Header } from "./components/header";
-import DefaultExport from "./DefaultExport";
-import { NamedExport } from "./NamedExport";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
+
 import "./global.css";
 import styles from './App.module.css'
 
@@ -10,18 +10,21 @@ function App() {
   return (
     <div>
       <Header/>
-      <DefaultExport/>
-      <DefaultExport/>
-      <NamedExport/>
-      <NamedExport/>
-      <Post
-        author="Mateus Zuffo"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur in nemo voluptas ut eius laboriosam perspiciatis molestias repellendus inventore corrupti velit, nobis voluptates officia ipsam vero, expedita earum, qui culpa?"
-      />
-      <Post
-        author="Mateus Zuffo"
-        content="Teste ipsum dolor sit amet consectetur adipisicing elit. Pariatur in nemo voluptas ut eius laboriosam perspiciatis molestias repellendus inventore corrupti velit, nobis voluptates officia ipsam vero, expedita earum, qui culpa?"
-      />
+
+      <div className={styles.wrapper}>
+
+      <Sidebar/>
+      <main>
+        <Post
+          author="Mateus Zuffo"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur in nemo voluptas ut eius laboriosam perspiciatis molestias repellendus inventore corrupti velit, nobis voluptates officia ipsam vero, expedita earum, qui culpa?"
+        />
+        <Post
+          author="Mateus Zuffo"
+          content="Teste ipsum dolor sit amet consectetur adipisicing elit. Pariatur in nemo voluptas ut eius laboriosam perspiciatis molestias repellendus inventore corrupti velit, nobis voluptates officia ipsam vero, expedita earum, qui culpa?"
+        />
+      </main>  
+      </div>
     </div>
   )
 }
--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="logotipo do Ignite"/>
     </header>
   )
 }
