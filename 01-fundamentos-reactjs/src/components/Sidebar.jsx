a7e9ef2880d4bd393a149b5ae4f7636a5b1bd8a6
Mateus Zuffo <mateushmdz@gmail.com>
2023-03-31 22:03:24 -0300

    FEAT - Avatar component created

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
 
@@ -9,7 +10,7 @@ export function Sidebar(){
             src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"  
         />
         <div className={styles.profile}>
-            <img className={styles.avatar} src="https://github.com/mateus-zuffo.png"/>
+            <Avatar src="https://github.com/mateus-zuffo.png"/>
             <strong>Mateus Zuffo</strong>
             <span>Web Developer</span>
         </div>
