3a806377f5800bc0aecdfb77ab4235c247518356
Mateus Zuffo <mateushmdz@gmail.com>
2023-04-05 23:25:41 -0300

    Feat - added iteration in comments on Post

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -3,12 +3,45 @@ import { Comment } from './comment';
 import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 import { ptBR } from 'date-fns/locale';
+import { useState } from 'react';
 
 const tipoAvatar = 'Post';
 
+
 export function Post({author, publishedAt, content}){
+    const [comments, setComments] = useState([
+        {   
+            id: 1,
+            name: 'Diego Fernandes',
+            url_avatar: 'https://github.com/diego3g.png',
+            comment: 'Muito bom, parabens!',
+        },
+        {   
+            id: 2,
+            name: 'Jose',
+            url_avatar: 'https://github.com/jose.png',
+            comment: 'Ta incrível, 10/10',
+        },
+    ]);
+    const [newCommentText, setNewCommentText] = useState('');
+
+    function handleNewCommentChange(){
+        setNewCommentText(event.target.value);
+    }
     const publishedAtFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm",{locale:ptBR});
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {locale: ptBR, addSuffix:true });
+    function handleCreateNewComment(){
+        event.preventDefault();
+        const newComment = {
+            id: comments.length+1,
+            name: 'Desconhecido',
+            url_avatar: 'https://st2.depositphotos.com/1007566/11569/v/950/depositphotos_115690172-stock-illustration-businessman-profile-colorful-avatar-over.jpg',
+            comment: newCommentText,
+        }
+
+        setComments([...comments, newComment]);
+        setNewCommentText('');
+    } 
 
     return (
         <article className={styles.post}>
@@ -31,18 +64,23 @@ export function Post({author, publishedAt, content}){
                     }
                 })} 
             </div>
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong> Deixe seu feedback</strong>
-                <textarea placeholder="Deixe um comentário">
+                <textarea 
+                    name="comment" 
+                    placeholder="Deixe um comentário"
+                    onChange={handleNewCommentChange}
+                    value={newCommentText}
+                />
                     
-                </textarea>
                 <footer>
                     <button type="submit">Comentar</button>
                 </footer>
             </form>
             <div className={styles.commentList}>
-                <Comment tipoAvatar='Comment' src="https://github.com/diego3g.png" nome='Diego Fernandes' comentario='Muito bom, parabéns!'/>
-                <Comment tipoAvatar='Comment' src="https://github.com/jose.png" nome='Jose' comentario='Ta incrível, 10/10'/>
+                {comments.map(each=>{
+                    return <Comment tipoAvatar='Comment' src={each.url_avatar} nome={each.name} comentario={each.comment}/>
+                })}
             </div>
         </article>
     )
